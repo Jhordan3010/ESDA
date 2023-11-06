3b5541d437cc9dc4e3a2c590fcdb39aeb9b380f8
Jhordan3010 <jmhueran@utn.edu.ec>
2023-11-06 11:26:33 -0500

    Nuevo

--- PRUEBA/src/prueba/PRUEBA.java
+++ PRUEBA/src/prueba/PRUEBA.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package prueba;
-
-/**
- *
- * @author jhord
- */
 public class PRUEBA {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a,b;
+        a =5;
+        b = 6;
+        int r = a +b;
+        System.out.println(r);
     }
     
 }
