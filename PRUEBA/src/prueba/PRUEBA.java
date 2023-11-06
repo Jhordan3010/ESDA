27a18f8baee176d410491a83574379e6e977604e
Jhordan3010 <jmhueran@utn.edu.ec>
2023-11-06 12:12:53 -0500

    Update PRUEBA.java

--- PRUEBA/src/prueba/PRUEBA.java
+++ PRUEBA/src/prueba/PRUEBA.java
@@ -3,14 +3,7 @@ package prueba;
 public class PRUEBA {
     public static void main(String[] args) {
         // TODO code application logic here
-        int a,b;
-        a =5;
-        b = 6;
-        int r = a +b;
-        System.out.println(r);
-        System.out.println("Hola");
-        System.out.println(a-b);
-        System.out.println("Hello word");
+        
     }
     
 }
