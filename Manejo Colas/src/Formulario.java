9211a2d9cb39a73fd98b22ecae83883e74606df8
Jhordan3010 <jmhueran@utn.edu.ec>
2023-11-07 11:24:22 -0500

    Comprar Vender y Agotado

--- Manejo Colas/src/Formulario.java
+++ Manejo Colas/src/Formulario.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -50,6 +53,9 @@ public class Formulario extends javax.swing.JFrame {
         txtMinimo = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         txtaResultado = new javax.swing.JTextArea();
+        btnComprar = new javax.swing.JButton();
+        btnVender = new javax.swing.JButton();
+        btnAgotar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -122,6 +128,27 @@ public class Formulario extends javax.swing.JFrame {
         txtaResultado.setRows(5);
         jScrollPane1.setViewportView(txtaResultado);
 
+        btnComprar.setText(" Comprar");
+        btnComprar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnComprarActionPerformed(evt);
+            }
+        });
+
+        btnVender.setText("Vender");
+        btnVender.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVenderActionPerformed(evt);
+            }
+        });
+
+        btnAgotar.setText("Agotar");
+        btnAgotar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgotarActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -136,16 +163,11 @@ public class Formulario extends javax.swing.JFrame {
                         .addComponent(jLabel2))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jLabel4))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel5))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel7))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel8)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel8))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
@@ -158,6 +180,15 @@ public class Formulario extends javax.swing.JFrame {
                         .addComponent(btnCrear)
                         .addGap(91, 91, 91))
                     .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnNumElementos)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnReprote)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnComprar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnVender)
+                        .addGap(0, 108, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(txtPrecio, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
@@ -167,24 +198,20 @@ public class Formulario extends javax.swing.JFrame {
                             .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txtMinimo, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(62, 62, 62)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnNumElementos)
-                                .addGap(18, 18, 18)
-                                .addComponent(btnReprote))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnEncolar)
-                                .addGap(18, 18, 18)
-                                .addComponent(btnDesencolar)
-                                .addGap(18, 18, 18)
-                                .addComponent(btnVacia)
-                                .addGap(18, 18, 18)
-                                .addComponent(btnLlena)))
-                        .addGap(0, 42, Short.MAX_VALUE))))
+                        .addComponent(btnEncolar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnDesencolar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnVacia)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnLlena)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnAgotar)
+                        .addGap(25, 25, 25))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -194,13 +221,13 @@ public class Formulario extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jLabel2)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(btnCrear)
+                    .addComponent(txtTamanio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel3)
-                            .addComponent(btnCrear)
-                            .addComponent(txtTamanio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4)
                             .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -219,18 +246,21 @@ public class Formulario extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel8)
-                            .addComponent(txtMinimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(txtMinimo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(31, 31, 31)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnEncolar)
+                            .addComponent(btnDesencolar)
+                            .addComponent(btnVacia)
+                            .addComponent(btnLlena)
+                            .addComponent(btnAgotar))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnNumElementos)
+                            .addComponent(btnReprote)
+                            .addComponent(btnComprar)
+                            .addComponent(btnVender)))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(31, 31, 31)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnEncolar)
-                    .addComponent(btnDesencolar)
-                    .addComponent(btnVacia)
-                    .addComponent(btnLlena))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnNumElementos)
-                    .addComponent(btnReprote))
                 .addContainerGap(27, Short.MAX_VALUE))
         );
 
@@ -309,6 +339,91 @@ public class Formulario extends javax.swing.JFrame {
         this.txtaResultado.setText(s);
     }//GEN-LAST:event_btnReproteActionPerformed
 
+    private void btnComprarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnComprarActionPerformed
+        // TODO add your handling code here:
+        String codigo = txtCodigo.getText();
+        int cantidad = Integer.parseInt(txtCantidad.getText());
+
+        boolean encontrado = false;
+        colAux = new Cola(col.getTam());
+
+        while (!col.Vacia()) {
+            Object r = col.Desencolar();
+            colAux.Encolar(r);
+            if (r instanceof Producto) {
+                Producto p = (Producto) r;
+                if (p.getCodigo().equals(codigo)) {
+                    p.setCantidad(p.getCantidad() + cantidad);
+                    encontrado = true;
+                }
+            }
+        }
+        col = colAux;
+
+        //Mostramos la actualizacion
+        if (encontrado) {
+            this.txtaResultado.setText("Se actualizó la cantidad del producto " + codigo);
+            this.txtCodigo.setText("");
+            this.txtCantidad.setText("");
+            this.txtaResultado.setText(col.Reporte());
+        } else {
+            this.txtaResultado.setText("El producto " + codigo + " no está en la cola");
+        }
+    }//GEN-LAST:event_btnComprarActionPerformed
+
+    private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
+        // TODO add your handling code here:
+        String codigo = txtCodigo.getText();
+        int cantidadVenta = Integer.parseInt(txtCantidad.getText());
+        int ne = col.getNe();
+        boolean verificar = false;
+
+        Producto prod;
+        for (int i = 0; i < ne; i++) {
+            prod = (Producto) col.Desencolar();
+            col.Encolar(prod);
+            if (codigo.equals(prod.getCodigo())) {
+                if (cantidadVenta > prod.getCantidad()) {
+                    JOptionPane.showMessageDialog(null, "Inventario insuficiente");
+                    verificar = true;
+                    break;
+                } else {
+                    prod.setCantidad(prod.getCantidad() - cantidadVenta);
+                    JOptionPane.showMessageDialog(null, "Venta registrada correctamente");
+                    verificar = true;
+                    break;
+                }
+            }
+        }
+        if (!verificar) {
+            JOptionPane.showMessageDialog(null, "No se encontró el código");
+        }
+    }//GEN-LAST:event_btnVenderActionPerformed
+
+    private void btnAgotarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgotarActionPerformed
+        // TODO add your handling code here:
+        Cola aux;
+        aux = new Cola(col.getTam());
+        Producto pro;
+        String resultado = "";
+        while (!col.Vacia()) {
+            pro = (Producto) col.Desencolar();
+            aux.Encolar(pro);
+            if (pro.getCantidad() <= pro.getMinimo() || pro.getCantidad() <= 0) {
+                resultado += "-->Este producto esta a punto de agotarse: \n"
+                        + "Còdigo: " + pro.getCodigo()
+                        + "\nNombre: " + pro.getNombre()
+                        + "\nCantidad: " + pro.getCantidad()
+                        + "\nPrecio " + pro.getPrecio()
+                        + "\nMinimo: " + pro.getMinimo() + "\n\n";
+            }
+        }
+        while (!aux.Vacia()) {
+            col.Encolar(aux.Desencolar());
+        }
+        this.txtaResultado.setText(resultado);
+    }//GEN-LAST:event_btnAgotarActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -345,6 +460,8 @@ public class Formulario extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAgotar;
+    private javax.swing.JButton btnComprar;
     private javax.swing.JButton btnCrear;
     private javax.swing.JButton btnDesencolar;
     private javax.swing.JButton btnEncolar;
@@ -352,6 +469,7 @@ public class Formulario extends javax.swing.JFrame {
     private javax.swing.JButton btnNumElementos;
     private javax.swing.JButton btnReprote;
     private javax.swing.JButton btnVacia;
+    private javax.swing.JButton btnVender;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
